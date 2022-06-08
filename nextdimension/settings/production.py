9658eac84b07e4a0bae2c9e5322532b9c0de0fba
Thiago Luiz Benevides <benevides.thiago@gmail.com>
2022-06-07 23:22:39 -0300

    Add files via upload

---
+++ nextdimension/settings/production.py
@@ -0,0 +1,13 @@
+from .settings import *
+
+DEBUG = False
+
+SECRET_KEY = "qpQ19}6?X+HU>sCUY2x,MLAB"
+ALLOWED_HOSTS = ['127.0.0.1']
+
+DATABESE= {
+    'default': {
+        'ENGINE': 'db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+    }
+}
